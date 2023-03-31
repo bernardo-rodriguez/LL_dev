b34b76f05db511072396e225496bd4236b60a86c
Bernardo Rodriguez <brodriguez@atlassian.com>
2023-03-31 12:23:12 -0400

    first commit

---
+++ assets/cookie-specific-actions.js
@@ -0,0 +1,88 @@
+function getCookie(cname) {
+  // const value = `; ${document.cookie}`;
+  // const parts = value.split(`; ${name}=`);
+  // if (parts.length === 2) return parts.pop().split(';').shift();
+  
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
+
+function setCookie(key, value) {
+  var date = new Date();
+  date.setFullYear(date.getFullYear() + 1);
+  var expires = date.toUTCString();
+  document.cookie = `${key}=${value}; expires=${expires}; path=/`;
+}
+
+function showAnnouncementBar(bar_text) {
+    $('#announcement-bar').css('display', 'block')
+    $(".outer-header-wrapper").css('top', '24px')
+    $(".announcement-bar p").html(bar_text)
+    $(window).scroll(function(){
+      height = $(window).scrollTop()
+      if (height > 24) {
+        $(".outer-header-wrapper").css('top', '0')
+      } else {
+        diff = 24 - height
+        $(".outer-header-wrapper").css('top', diff + 'px')
+      }
+    });
+}
+
+
+function cookie_actions() {
+    const cookies = ['redirect_ut', 'redirect_ut_direct', 'redirect_paceline', 'redirect_sweatcoin', 'redirect_miles', 'redirect_studentbeans']
+
+    let subscriptionCookie = cookies.filter( cookieName => getCookie(cookieName) != null )
+
+    switch(subscriptionCookie[0]) {
+      case 'redirect_sweatcoin':
+        showAnnouncementBar('👋 Sweatcoin Users! Discount auto applied at checkout')
+        break
+      case 'redirect_ut':
+        showAnnouncementBar('Free Shipping + Discount auto applied at checkout')
+        break
+    }
+
+    quiz_version = getCookie('quiz_version')
+    
+    if (quiz_version == null) {
+      var d = Math.random();
+      if (d <= .5) {
+          setCookie('quiz_version', 'long')
+          gtag('set', 'user_properties', {
+            quiz_version: "long"
+          });
+      } else {
+          setCookie('quiz_version', 'short')
+          gtag('set', 'user_properties', {
+            quiz_version: "short"
+          });
+      }
+    } else {
+      if (quiz_version == 'long') {
+          gtag('set', 'user_properties', {
+            quiz_version: "long"
+          });
+      } else if (quiz_version == 'short') {
+          gtag('set', 'user_properties', {
+            quiz_version: "short"
+          });
+      }
+    }
+}
+
+
+cookie_actions()
+
---
+++ assets/quiz_2.js
@@ -0,0 +1,167 @@
+customElements.define('formula-quiz-2', class FormulaQuiz2 extends HTMLElement {
+  static get observedAttributes() {
+    return ["data-state"];
+  }
+  constructor() {
+    super();  
+    this.open = document.querySelectorAll("[data-open-quiz-2]")
+    this.close = this.querySelector(".quiz_2__close")
+    this.headlines = this.querySelectorAll(".quiz_2__headline")
+    this.quizCards = this.querySelectorAll(".form__step-wrapper_2")
+
+    this.currentStep = this.dataset.state
+    this.allInputs = this.querySelectorAll(`.form__step-wrapper_2 input`)
+
+    // this.inputs = this.querySelectorAll(`.form__step-wrapper_2[data-step='${this.currentStep}'] input`)
+    // this.inputNames = []
+    // Array.from(this.inputs).forEach((i) => {if (!this.inputNames.includes(i.name)) this.inputNames.push(i.name)})
+    // console.log(this.inputNames)
+
+    this.setInputs(this.currentStep)
+
+    this.back = this.querySelector("[data-form-back]")
+    this.next = this.querySelector("[data-form-next]")
+    this.submit = this.querySelector("[data-form-submit]")
+
+    this.form = this.querySelector(".quiz_2__form")
+
+    this.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.closeQuiz.bind(this))
+
+    this.allInputs.forEach((input) => {
+      input.addEventListener('change', function(e){
+        if(this.validateFormStep(this.inputs) == true) {
+          this.next.removeAttribute('disabled')
+        } else {
+          this.next.setAttribute('disabled', 'true')
+        }
+      }.bind(this))
+    })
+
+    this.bindEvents();
+  }
+  
+  bindEvents() {
+    this.open.forEach((button) => {
+      button.addEventListener("click", function(e){
+        e.preventDefault();
+        if(document.querySelector("body").classList.contains("Menu_Open")) {
+          document.querySelector("menu-drawer").closeMenuDrawer()
+        }
+        document.querySelector('.sticky-footer__button')?.click()
+      } )
+    })
+    this.back.addEventListener('click', this.changeFormStep.bind(this, -1))
+    this.next.addEventListener('click', this.changeFormStep.bind(this, 1))
+    this.close.addEventListener('click', this.closeQuiz.bind(this))
+    this.submit.addEventListener('click', function(e){
+      this.submitForm(e)
+    }.bind(this))
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (newValue !== oldValue && oldValue != null ) {
+      this.querySelector(`.quiz_2__headline[data-step="${ oldValue }"`).classList.remove('active')
+      this.querySelector(`.quiz_2__headline[data-step="${ newValue }"`).classList.add('active')
+
+      this.querySelector(`.form__step-wrapper_2[data-step="${ oldValue }"`).classList.remove('active')
+      this.querySelector(`.form__step-wrapper_2[data-step="${ newValue }"`).classList.add('active')
+
+      if( newValue == 3 ){
+        this.next.classList.toggle('hidden')
+        this.submit.classList.remove('hidden')
+      }
+      if ( newValue == 2 && oldValue == 3 ){
+        this.next.classList.toggle('hidden')
+        this.submit.classList.add('hidden')
+      }
+    }
+  }
+
+  closeQuiz() {
+    this.querySelector('.quiz_2').style.transitionDelay = '.2s'
+    this.querySelector('.quiz_2').setAttribute('aria-hidden', 'true')
+    document.querySelector('.page-transition__content').style.transform = 'translate(-50%, 50%)'
+    setTimeout(() => {
+      document.querySelector('.page-transition').classList.toggle('visible');
+      document.querySelector('.sticky-footer__button').classList.toggle('hide')
+      document.querySelector('.page-transition__content').style.removeProperty('transform')
+      this.querySelector('.quiz_2').style.removeProperty('transition-delay')
+    }, 900)
+  }
+
+  changeFormStep(x) {
+    let currentState = this.dataset.state
+    let newState = currentState
+    if ( x === 1  && currentState < 5 ){
+      newState = ++currentState
+    } else if ( x === -1 && currentState > 1) {
+      newState = --currentState
+    }
+
+    this.setAttribute('data-state', newState )
+
+    this.setInputs(newState)
+    if(this.validateFormStep(this.inputs) == true) {
+      this.next.removeAttribute('disabled')
+    } else {
+      this.next.setAttribute('disabled', 'true')
+    }
+  }
+
+  setInputs(step) {
+    this.inputs = this.querySelectorAll(`.form__step-wrapper_2[data-step='${step}'] input`)
+    this.inputNames = []
+    Array.from(this.inputs).forEach((i) => {if (!this.inputNames.includes(i.name)) this.inputNames.push(i.name)})
+  }
+
+  validateFormStep(inputs) {
+    let validationCheck = true
+    for( const value of this.inputNames.values()){
+      const i = this.querySelector(`[name='${value}']`)
+      if( i.getAttribute('type') == 'checkbox' || i.getAttribute('type') == 'radio' ) {
+
+        if(this.querySelector(`[name='${value}']:checked`)?.value == "" || this.querySelector(`[name='${value}']:checked`)?.value == undefined ){
+          validationCheck = false
+          break
+        }
+      } else {
+
+        if(this.querySelector(`[name='${value}']`)?.value == "" || this.querySelector(`[name='${value}']`)?.value == undefined ){
+          validationCheck = false
+          break
+        }
+      }
+    }
+    return validationCheck
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    this.submit.classList.add("loading")
+    let sensitivity = document.querySelector('input[name="sensitivity_2"]:checked')?.value || 'medium'
+
+    let formula_translate = {
+      'mild_2' : 'strong',
+      'medium_2': 'medium',
+      'very_2': 'sensitive'
+    }
+
+    document.cookie =  "strength=" + formula_translate[sensitivity] + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;"
+    document.cookie = "firstname=" + document.querySelector('#first_name_2').value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;"
+    document.cookie = "lastname=" + document.querySelector('#last_name_2').value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;"
+
+    let ut = getCookie('redirect_ut')
+    let ut_direct = getCookie('redirect_ut_direct')
+
+    setTimeout(function(){
+      console.log(document.cookie)
+
+      if (ut == 'true' && ut_direct != 'true') {
+        window.location = '/products/at-home-whitening-kit'
+      } else {
+        window.location = '/products/at-home-whitening-kit'
+      }
+    }, 1000);
+
+  }
+})
