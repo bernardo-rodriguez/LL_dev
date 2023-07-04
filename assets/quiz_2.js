42e7fe2008e51bff4aaf95cc6a4fbb1a743b08fa
Bernardo Rodriguez <brodriguez@atlassian.com>
2023-07-04 00:55:27 -0400

    df

--- assets/quiz_2.js
+++ assets/quiz_2.js
@@ -323,6 +323,7 @@ customElements.define('formula-quiz-2', class FormulaQuiz2 extends HTMLElement {
       function() {
         $('.quiz_2__content-buttons').show()
         that.changeFormStep(1)
+        elem.style.width = "1%";
       }, 4000);
   }
 
