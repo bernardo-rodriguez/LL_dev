3afa3c8e4118a3f859e196bbd2de6190ef5d53b4
Bernardo Rodriguez <brodriguez@atlassian.com>
2023-09-21 21:13:43 -0700

    d

--- assets/product-landing-populate.js
+++ assets/product-landing-populate.js
@@ -107,20 +107,20 @@ function setTeethShading(starting_shade, ending_shade) {
     console.log(second_jump)
     console.log(third_jump)
     setTimeout(() => {
-        document.getElementById('mobile_journey_tooth_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('mobile_journey_tooth_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('mobile_journey_tooth_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('mobile_journey_tooth_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_mobile_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_mobile_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_mobile_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_mobile_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
 
-        document.getElementById('ipad_journey_tooth_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('ipad_journey_tooth_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('ipad_journey_tooth_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('ipad_journey_tooth_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
 
-        document.getElementById('desktop_journey_tooth_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('desktop_journey_tooth_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('desktop_journey_tooth_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
-        document.getElementById('desktop_journey_tooth_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_1').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(starting_shade/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_2').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(first_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_3').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(second_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
+        document.getElementById('journey_boxes_tablet_4').setAttribute('style', 'fill: hsl(43,40%,' + Math.min(third_jump/ 3 + 70) + '%); mix-blend-mode:multiply;');
       }, 3000);
 }
 
