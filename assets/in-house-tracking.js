ce1afdfb00fcfe57d7cae4deeae44f72db354290
Bernardo Rodriguez <brodriguez@atlassian.com>
2023-11-15 16:21:55 -0800

    remoe timout

--- assets/in-house-tracking.js
+++ assets/in-house-tracking.js
@@ -10,7 +10,7 @@ function LandingPopulateCactus() {
   document.getElementsByClassName('Hero_Subtitle')[0].querySelector('span').style.fontSize = '22px'
 
   // $('.Hero_Subtitle').first().find('.stylized').first().html("<sup>$</sup>0")
-  $('.Hero_Subtitle').first().text("sup")
+  $('.Hero_Subtitle').first().text("See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.")
 }
 
 
