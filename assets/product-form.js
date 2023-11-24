ee4a33159f9cee7e3c8b99c771512c97ba1d42b7
Bernardo Rodriguez <brodriguez@atlassian.com>
2023-11-23 22:52:51 -0800

    d

--- assets/product-form.js
+++ assets/product-form.js
@@ -311,7 +311,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         // subPrice = '$0'
         // subText = 'STARTER TRIAL'
         subPrice = '$19'
-        subText = 'FREE PEN'
+        subText = 'STARTER SPECIAL'
         this.setToOneMonth()
         break;
       case 'redirect_ut_direct':
