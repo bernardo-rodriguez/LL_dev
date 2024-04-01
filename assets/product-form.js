7d8dea6ebc65ad87e88fc47076775947a4126d74
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-04-01 07:28:19 -0700

    free cactus kit

--- assets/product-form.js
+++ assets/product-form.js
@@ -354,10 +354,10 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         break
       case 'redirect_ut':
         // add_pen()
-        subPrice = '$9'
-        subText = 'STARTER SPECIAL'
-        // subPrice = '$0'
-        // subText = 'FREE TRIAL SPECIAL'
+        // subPrice = '$9'
+        // subText = 'STARTER SPECIAL'
+        subPrice = '$0'
+        subText = 'FREE TRIAL SPECIAL'
         // subPrice = '$19'
         // subText = 'STARTER SPECIAL'
         this.setToOneMonth()
