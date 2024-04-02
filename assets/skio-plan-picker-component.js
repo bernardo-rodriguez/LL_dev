0896ab48a455c453fb489b8264892b3e7543070b
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-04-01 23:42:11 -0700

    nested question marks

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -551,7 +551,7 @@ export class SkioPlanPickerComponent extends LitElement {
                   </div>
                   <div class="skio-center-wrapper">
                     <div class="skio-group-title">
-                    ${ group.name == 'Subscription' ? 'Free Trial Special' : group.name }
+                    ${ group.name == 'Subscription' ?  selling_plan.price_adjustments[0].value == 0 ? 'Free Trial Special' : 'Subscribe & Save' : group.name }
                       ${ this.discount(group.selected_selling_plan).percent !== '0%' ? html`
                         <span style="display: none;" class="skio-save">Save <span skio-discount>${ this.discountFormat == 'percent' ? this.discount(group.selected_selling_plan).percent : this.discount(group.selected_selling_plan).amount }</span></span>
                       ` : html`` }
