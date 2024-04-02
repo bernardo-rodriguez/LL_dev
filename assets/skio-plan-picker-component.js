13eb1542e028c6e804aa5713aef68400fe65f8d8
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-04-02 08:47:33 -0700

    1 month supply

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -573,7 +573,7 @@ export class SkioPlanPickerComponent extends LitElement {
                 <div class="skio-group-content">
                   <div class="skio-custom-content">
                     <div class="skio-container">
-                      <div>Whitening Gels</div>
+                      <div>${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' ? '1 Month Supply' : 'Whitening Gels' }</div>
                       <div>Refills for $25/mo</div>
                     </div>
                     <div class="skio-container" style = "margin-top: 10px">
