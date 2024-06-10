ccf14a2429aa1b7654b1ace06e870c5fb9c84e77
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-06-10 11:37:24 -0700

    d

--- assets/product-form.js
+++ assets/product-form.js
@@ -427,17 +427,19 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
        //  oneTimePrice = '$57'
        //  break
        case 'redirect_sweatcoin':
-        subPrice = '$9'
+        subPrice = '$0'
         subText = 'SWEATCOIN SPECIAL'
         // this.setToOneMonth()
-        add_pen()
+        this.add_pen()
+
+        // add_pen()
         break
       case 'redirect_ut':
-        // add_pen()
+        // this.add_pen()
         // subPrice = '$9'
-        // subText = 'STARTER SPECIAL'
-        subPrice = '$0'
-        subText = 'FREE TRIAL SPECIAL'
+        subText = 'STARTER SPECIAL'
+        subPrice = '$9'
+        // subText = 'FREE TRIAL SPECIAL'
         // subPrice = '$19'
         // subText = 'STARTER SPECIAL'
         // this.setToOneMonth()
