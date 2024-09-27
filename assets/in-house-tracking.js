655aa483de0193a72064a675394c695a908aab4c
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-09-27 16:39:14 -0400

    cleanup in house tracking

--- assets/in-house-tracking.js
+++ assets/in-house-tracking.js
@@ -101,39 +101,26 @@ function setCookieIfFirstTime() {
         }
       }
       setCookie('cookie_hasnt_been_set', 'true')
-
-      if (d < .5) {
-        gtag('set', 'user_properties', {
-          test_random: "1"
-        });
-      } else {
-        gtag('set', 'user_properties', {
-          test_random: "2"
-        });
-      }
   }
 }
 
 
 function redirectToLandingIfFirstTime2(cookie, p) {
   var d = Math.random();
-  if (d <= 0) {
-    setGoogleLanding('homepage')
-  } else {
-      console.log('rand' + d)
-      console.log('set' + p)
-      if (d <= p) {
-        console.log('flow 1')
-        setCookie('flow_ga_tracking_v1', 'true')
-      } else {
-        console.log('flow 2')
-        setCookie('flow__ga_tracking_v2', 'true')
-      }
 
-    setCookie('cookie_tracked', 'true')
-    if (getCookie("in_house_already_redirected") != 'true') {
-      setCookie('in_house_already_redirected', 'true')
-    }
+  console.log('rand' + d)
+  console.log('set' + p)
+  if (d <= p) {
+    console.log('flow 1')
+    setCookie('flow_ga_tracking_v1', 'true')
+  } else {
+    console.log('flow 2')
+    setCookie('flow__ga_tracking_v2', 'true')
+  }
+
+  setCookie('cookie_tracked', 'true')
+  if (getCookie("in_house_already_redirected") != 'true') {
+    setCookie('in_house_already_redirected', 'true')
   }
 }
 
@@ -145,85 +132,25 @@ function redirectToLandingIfFirstTime(cookie, from_landing=false) {
   //   setCookie('in_house_already_redirected', 'true')
               
   var d = Math.random();
-  if (d <= 0) {
-    setGoogleLanding('homepage')
-    // window.location.href = 'https://www.mylaughland.com'
+  tracking_1 = parseInt(document.getElementById('tracking_v1').innerHTML) / 100
+  if (d <= tracking_1) {
+    console.log('flow 1')
+    setCookie('flow_ga_tracking_v1', 'true')
   } else {
-      tracking_1 = parseInt(document.getElementById('tracking_v1').innerHTML) / 100
-      if (d <= tracking_1) {
-        console.log('flow 1')
-        setCookie('flow_ga_tracking_v1', 'true')
-      } else {
-        console.log('flow 2')
-        setCookie('flow__ga_tracking_v2', 'true')
-      }
-
-    setCookie('cookie_tracked', 'true')
-    if (getCookie("in_house_already_redirected") != 'true' && from_landing != true) {
-      setCookie('in_house_already_redirected', 'true')
-
-      setGoogleLanding('landing-page')
-      // setTimeout(function(){
-      //    window.location.href = 'https://www.mylaughland.com/pages/landing-page'
-      // }, 200);
-    }
+    console.log('flow 2')
+    setCookie('flow__ga_tracking_v2', 'true')
   }
-  // }
-}
 
+  setCookie('cookie_tracked', 'true')
+  if (getCookie("in_house_already_redirected") != 'true' && from_landing != true) {
+    setCookie('in_house_already_redirected', 'true')
 
-function setInHouseTracked() {
-  gtag('set', 'user_properties', {
-    in_house_tracked: "true"
-  });
-}
-
-
-function setInHouseTracked2() {
-  gtag('set', 'user_properties', {
-    in_house_tracked_2: "true"
-  });
-}
-
-
-function setLandingPageFlag(flag_value) {
-  gtag('set', 'user_properties', {
-    landing_page_flag: flag_value
-  });
-}
-
-
-function setGoogleSourceDev2(affiliate_source_dev) {
-  gtag('set', 'user_properties', {
-    affiliate_source_dev_2: affiliate_source_dev
-  });
-}
-
-
-function setGoogleSourceDev(affiliate_source_dev) {
-  gtag('set', 'user_properties', {
-    affiliate_source_dev: affiliate_source_dev
-  });
-}
-
-
-function setGoogleSource(affiliate_source) {
-  gtag('set', 'user_properties', {
-    affiliate_source: affiliate_source
-  });
-}
-
-
-function setGoogleLanding(effective_landing_page) {
-  gtag('set', 'user_properties', {
-    effective_landing_page: effective_landing_page
-  });
+  }
 }
 
 
 function setCookieAffiliate(cookie, affiliate) {
   console.log("swetcookieaffiliate")
-  setGoogleSource(affiliate)
   setCookie(cookie, 'true')
 
   // remove all other affiliate cookies except for the one I'm setting
@@ -238,105 +165,64 @@ function setCookieAffiliate(cookie, affiliate) {
 }
 
 
-function setFirstTimeGtags(affiliate) {
-  setInHouseTracked2() // set in_house_tracked_2
-  setGoogleSourceDev2(affiliate) // set affiliate_source_dev_2
-  setCookie('in_house_tracked', 'true')
-  if (getCookie("in_house_tracked") != 'true') {
-    setInHouseTracked()
-    setGoogleSourceDev(affiliate)
-  }
-}
-
-
 function landingPageAction(current_page, query_params) {
   // This gets callled on every page visited (script type defer)
   // curent_page: page without query parameters (',', 'pages/landing-page')
   // query_params: dictionary of all query parameters (null if not found)
-  if (query_params.quiz_version == 'base_v1') {
-    setCookie('quiz_version', 'base_v1')
-    gtag('set', 'user_properties', {
-      quiz_version: "base_v1"
-    });
-  } else if (query_params.quiz_version == 'interactive_v1') {
-    setCookie('quiz_version', 'interactive_v1')
-    gtag('set', 'user_properties', {
-      quiz_version: "interactive_v1"
-    });
-  }
   console.log("tracker:")
   console.log(current_page)
   if (current_page == '/') {
     switch(query_params.utm_affiliate_specific) {
       case 'sweatcoin':
         setCookieAffiliate('redirect_sweatcoin', 'Sweatcoin')
-        setFirstTimeGtags('Sweatcoin')
         redirectToLandingIfFirstTime('redirect_sweatcoin')
         break;
       case 'product-direct':
         setCookieAffiliate('redirect_ut_direct', 'Direct To Product (misc)')
-        setFirstTimeGtags('Direct To Product (misc)')
         redirectToLandingIfFirstTime('redirect_ut_direct')
         break;
       case 'paceline':
         setCookieAffiliate('redirect_paceline', 'Paceline')
-        setFirstTimeGtags('Paceline')
         redirectToLandingIfFirstTime('redirect_paceline')
         break;
       case 'miles':
         setCookieAffiliate('redirect_miles', 'Miles')
-        setFirstTimeGtags('Miles')
         redirectToLandingIfFirstTime('redirect_miles')
         break;
       case 'utm_partner':
         setCookieAffiliate('utm_partner', 'UTM Partner')
-        setFirstTimeGtags('UTM Partner')
         redirectToLandingIfFirstTime('utm_partner')
         break;
       case 'utm_gen_direct':
         setCookieAffiliate('utm_gen_direct', 'UTM Gen Direct')
-        setFirstTimeGtags('UTM Gen Direct')
         redirectToLandingIfFirstTime('utm_gen_direct')
         break;
       case 'cactus_media':
         setCookieAffiliate('redirect_ut', 'Cactus Media')
-        setFirstTimeGtags('Cactus Media')
         redirectToLandingIfFirstTime('redirect_ut')
         break;
       case 'redirect_inspire':
         setCookieAffiliate('redirect_inspire', 'Inspire More')
-        setFirstTimeGtags('Inspire More')
         redirectToLandingIfFirstTime('redirect_inspire')
         break;
       case 'redirect_pinterest':
         setCookieAffiliate('redirect_pinterest', 'Pinterest')
-        setFirstTimeGtags('Pinterest')
         redirectToLandingIfFirstTime2('redirect_pinterest', .5)
         break;
       case 'skimm':
         setCookieAffiliate('redirect_skimm', 'Skimm')
-        setFirstTimeGtags('Skimm')
         redirectToLandingIfFirstTime2('redirect_skimm', .25)
         setDefaultStrength(query_params)
         break;
       default:
-        // setCookie('redirect_sweatcoin', 'true')
-        setFirstTimeGtags('NA')
         setCookie('in_house_already_redirected', 'true')
         redirectToLandingIfFirstTime('none')
-        // setCookieAffiliate('redirect_sweatcoin', 'Sweatcoin')
-        // setFirstTimeGtags('Sweatcoin')
-        // redirectToLandingIfFirstTime('redirect_sweatcoin')
         break;
     } 
   } else if (current_page == '/pages/clear-affiliate-cookies') {
       clearAllAffiliateCookies()
-      removeCookie('in_house_tracked')
   } else if (current_page == '/pages/landing-page' || current_page == '/pages/landing-page/' || current_page.includes('landing-page')) {
       console.log("tracker_landing_page")
-      setLandingPageFlag('true')
-      setFirstTimeGtags('landing-page')
-      setGoogleLanding('landing-page')
       setCookie('in_house_already_redirected', 'true')
       if (query_params.utm_affiliate_specific == 'sweatcoin') {
         LandingPopulateSweatcoin()
@@ -357,18 +243,15 @@ function landingPageAction(current_page, query_params) {
       }
   } else {
       setCookie('in_house_already_redirected', 'true')
-      setFirstTimeGtags(current_page)
       if (query_params.utm_affiliate_specific == 'sweatcoin') {
         setCookieAffiliate('redirect_sweatcoin', 'Sweatcoin')
         setCookie('cookie_tracked', 'true')
       } else if (query_params.utm_affiliate_specific == 'skimm') {
         setCookieAffiliate('redirect_skimm', 'Skimm')
-        setFirstTimeGtags('Skimm')
         setCookie('cookie_tracked', 'true')
         setDefaultStrength(query_params)
       } else if (query_params.utm_affiliate_specific == 'redirect_pinterest') {
         setCookieAffiliate('redirect_pinterest', 'Pinterest')
-        setFirstTimeGtags('Pinterest')
         setCookie('cookie_tracked', 'true')
       } else if (query_params.utm_affiliate_specific == 'cactus_media') {
         setCookieAffiliate('redirect_ut', 'Cactus Media')
