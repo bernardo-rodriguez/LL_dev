463e761cbbe1d7a11e9d41a321fe48d5f7059a5d
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-10-30 10:36:38 -0400

    d

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -577,7 +577,6 @@ export class SkioPlanPickerComponent extends LitElement {
                       <div>${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? '1 Month Supply' : 'Refills for $25/mo' }</div>
                     </div>
                     <div class="skio-container" style = "margin-top: 10px">
-                      <div>Delivery Frequency</div>
                       <select skio-selling-plans="${ group.id }" class="skio-frequency${ group.selling_plans.length == 1 ? ' skio-frequency--one' : '' }
                       ${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? ' hide-skio-select' : '' }"
                         @change=${ (e) => this.selectSellingPlan(e.target, group) }>
