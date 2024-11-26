99292348ac54766ed8c4e976985cfc3f9321683a
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-11-25 23:37:55 -0500

    cactus copy

--- assets/product-form.js
+++ assets/product-form.js
@@ -178,12 +178,12 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         // this.add_pen()
         break
       case 'redirect_ut':
-        add_pen_cookie()
+        // add_pen_cookie()
         // this.add_pen()
-        subPrice = '$9'
-        subText = 'STARTER SPECIAL'
-        // subPrice = '$0'
-        // subText = 'FREE TRIAL SPECIAL'
+        // subPrice = '$9'
+        // subText = 'STARTER SPECIAL'
+        subPrice = '$0'
+        subText = 'FREE TRIAL SPECIAL'
         // subPrice = '$19'
         // subText = 'STARTER SPECIAL'
         break;
