9521830fd40fc6473ad4c0abadf463b309e72fb6
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-14 16:03:04 -0500

    dont hide, and click

--- assets/product-form.js
+++ assets/product-form.js
@@ -157,9 +157,9 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
 
         let refill_formula_selector = this.querySelector(`variant-radios input[value="${selection}"]`)
         console.log(refill_formula_selector)
-        // refill_formula_selector.click()
+        refill_formula_selector.click()
 
-        refill_formula_selector.setAttribute('checked', '');
+        // refill_formula_selector.setAttribute('checked', '');
         // refill_formula_selector.checked = true;
         // refill_formula_selector.dispatchEvent(new Event('change', { bubbles: true }));
 
