5593151a12ca77fbbf415eb7ea1abc0a6b805ab4
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-14 15:35:08 -0500

    try more stuff

--- assets/product-form.js
+++ assets/product-form.js
@@ -158,7 +158,10 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         let refill_formula_selector = this.querySelector(`variant-radios input[value="${selection}"]`)
         console.log(refill_formula_selector)
         refill_formula_selector.click()
+        
+        refill_formula_selector.setAttribute('checked', '');
         refill_formula_selector.checked = true;
+        refill_formula_selector.dispatchEvent(new Event('change', { bubbles: true }));
 
         if(document.querySelector(`[data-formula-type] [data-variant-title="${selection}"]`)){
           document.querySelector(`[data-formula-type] [data-variant-title="${selection}"]`).classList.remove("hidden")
