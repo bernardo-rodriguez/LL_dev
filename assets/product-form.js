13a7af8a832e0a720a36016a10d8d03de2461c5f
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-19 03:20:53 -0500

    waited for skio

--- assets/product-form.js
+++ assets/product-form.js
@@ -311,6 +311,18 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         this.observeForm(selling_plan_input)
         this.setVariant();
         this.observeFormulaPicker('input[name="refill-strength"]')
+
+        onetime_bundle_radio = document.querySelector('skio-plan-picker').shadowRoot.querySelectorAll('.bundle-container input[type="radio"]')
+        console.log('im here')
+        console.log(onetime_bundle_radio)
+        onetime_bundle_radio.forEach(radio => {
+          radio.addEventListener('change', (e) => {
+            console.log('onetime changed')
+            this.updateStickyBar(e)
+            this.mostRecentSellingPlan = e.detail.sellingPlan ? e.detail.sellingPlan.id : this.mostRecentSellingPlan
+            console.log(this.mostRecentSellingPlan)
+          });
+        });
       })
     } catch (e) {
         console.log("Error: failure in createSubcriptionWidget() for product-form.js")
@@ -584,18 +596,6 @@ customElements.define('sticky-product-bar', class StickyProductBar extends HTMLE
       this.sellingPlans.addEventListener("change", function(e){
         this.updateSellingPlans(e)
       }.bind(this))
-
-      onetime_bundle_radio = document.querySelector('skio-plan-picker').shadowRoot.querySelectorAll('.bundle-container input[type="radio"]')
-        console.log('im here')
-        console.log(onetime_bundle_radio)
-        onetime_bundle_radio.forEach(radio => {
-          radio.addEventListener('change', (e) => {
-            console.log('onetime changed')
-            this.updateStickyBar(e)
-            this.mostRecentSellingPlan = e.detail.sellingPlan ? e.detail.sellingPlan.id : this.mostRecentSellingPlan
-            console.log(this.mostRecentSellingPlan)
-          });
-        });
     })
 
     this.bindEvents()
