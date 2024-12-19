2bbe59401229b08ce962b7e13d879908da41410b
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-19 02:11:08 -0500

    bind events to bundle selector

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -863,6 +863,7 @@ export class SkioPlanPickerComponent extends LitElement {
         this.addVariantClickEventListeners()
       } else {
         console.log('this not refill product')
+        this.bind_one_time_bundle_selector()
       }
 
       //update key
@@ -992,6 +993,36 @@ export class SkioPlanPickerComponent extends LitElement {
       this.updateForm();
     }
 
+  }
+
+  bind_one_time_bundle_selector = () => {
+    const bundleOptions = document.querySelectorAll('.bundle-option');
+            
+    // Set initial selected state
+    const initiallySelected = document.querySelector('input[type="radio"]:checked');
+    if (initiallySelected) {
+        initiallySelected.closest('.bundle-option').classList.add('selected');
+    }
+    
+    bundleOptions.forEach(option => {
+        option.addEventListener('click', function(e) {
+            // Prevent default radio button behavior
+            e.preventDefault();
+            
+            // Select the radio button
+            const radio = this.querySelector('input[type="radio"]');
+            radio.checked = true;
+            
+            // Update selected states
+            bundleOptions.forEach(opt => {
+                opt.classList.remove('selected');
+            });
+            this.classList.add('selected');
+            
+            // Dispatch change event
+            radio.dispatchEvent(new Event('change', { bubbles: true }));
+        });
+      });
   }
 
   log = (...args) => {
