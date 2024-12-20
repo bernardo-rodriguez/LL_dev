da08853f477a2d9269297a2a4052aaa7b9ad3eed
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-20 14:02:52 -0500

    individual

--- assets/product-form.js
+++ assets/product-form.js
@@ -356,7 +356,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
     skio_plan_picker.addEventListener('click', (e) => {
       if (document.querySelector('[data-sticky-onetime]').classList.contains('selected')) {
         console.log(document.querySelector('skio-plan-picker').shadowRoot.querySelectorAll('input[name="onetime_bundle"]:checked'))
-        let customPrice = document.querySelector('skio-plan-picker').shadowRoot.querySelectorAll('input[name="onetime_bundle"]:checked').dataset.customPrice
+        let customPrice = document.querySelector('skio-plan-picker').shadowRoot.querySelector('input[name="onetime_bundle"]:checked').dataset.customPrice
         console.log(customPrice)
         document.querySelector(".sticky__price").innerHTML = price
       }
