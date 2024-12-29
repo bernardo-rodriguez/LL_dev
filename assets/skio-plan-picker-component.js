9fe0d9f57f199149684985a80f330009e1cbed0f
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-29 07:43:29 -0800

    pricing specific

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -730,7 +730,7 @@ export class SkioPlanPickerComponent extends LitElement {
                             </div>
                             <div class="bundle-pricing">
                               <div class="retail-price">Retail $299.97</div>
-                              <div class="sale-price">${ this.oneTimePricingConfig['first']['subtotal'] }</div>
+                              <div class="sale-price">${ this.oneTimePricingConfig['second']['subtotal'] }</div>
                               <div class="savings">Save $219.98</div>
                             </div>
                           </div>
@@ -750,7 +750,7 @@ export class SkioPlanPickerComponent extends LitElement {
                             </div>
                             <div class="bundle-pricing">
                               <div class="retail-price">Retail $499.95</div>
-                              <div class="sale-price">${ this.oneTimePricingConfig['first']['subtotal'] }</div>
+                              <div class="sale-price">${ this.oneTimePricingConfig['third']['subtotal'] }</div>
                               <div class="savings">Save $379.96</div>
                             </div>
                           </div>
