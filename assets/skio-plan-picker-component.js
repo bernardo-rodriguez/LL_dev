4b080c8ee5a475c3afb1563d294d109c93ed0cfd
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-30 10:46:35 -0800

    restructure

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -707,11 +707,11 @@ export class SkioPlanPickerComponent extends LitElement {
                           <div class="bundle-header">
                             <div>
                               <h3 class="bundle-title">${ this.oneTimePricingConfig['first']['offer_title'] }</h3>
-                              <div class="savings">${ this.oneTimePricingConfig['first']['save'] }</div>
+                              <div class="retail-price">${ this.oneTimePricingConfig['first']['retail_saving'] }</div>
                             </div>
                             <div class="bundle-pricing">
-                              <div class="retail-price">${ this.oneTimePricingConfig['first']['retail_saving'] }</div>
                               <div class="sale-price">${ this.oneTimePricingConfig['first']['subtotal'] }</div>
+                              <div class="savings">${ this.oneTimePricingConfig['first']['save'] }</div>
                             </div>
                           </div>
                         </div>
@@ -725,11 +725,11 @@ export class SkioPlanPickerComponent extends LitElement {
                           <div class="bundle-header">
                             <div>
                               <h3 class="bundle-title">${ this.oneTimePricingConfig['second']['offer_title'] }</h3>
-                              <div class="savings">${ this.oneTimePricingConfig['second']['save'] }</div>
+                              <div class="retail-price">${ this.oneTimePricingConfig['second']['retail_saving'] }</div>
                             </div>
                             <div class="bundle-pricing">
                               <div class="retail-price">${ this.oneTimePricingConfig['second']['retail_saving'] }</div>
-                              <div class="sale-price">${ this.oneTimePricingConfig['second']['subtotal'] }</div>
+                              <div class="savings">${ this.oneTimePricingConfig['second']['save'] }</div>
                             </div>
                           </div>
                         </div>
@@ -744,11 +744,11 @@ export class SkioPlanPickerComponent extends LitElement {
                           <div class="bundle-header">
                             <div>
                               <h3 class="bundle-title">${ this.oneTimePricingConfig['third']['offer_title'] }</h3>
-                              <div class="savings">${ this.oneTimePricingConfig['third']['save'] }</div>
+                              <div class="sale-price">${ this.oneTimePricingConfig['third']['subtotal'] }</div>
                             </div>
                             <div class="bundle-pricing">
                               <div class="retail-price">${ this.oneTimePricingConfig['third']['retail_saving'] }</div>
-                              <div class="sale-price">${ this.oneTimePricingConfig['third']['subtotal'] }</div>
+                              <div class="savings">${ this.oneTimePricingConfig['third']['save'] }</div>
                             </div>
                           </div>
                         </div>
