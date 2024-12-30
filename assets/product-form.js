a1251f84b2ac49ed3a34155e08a1d34a658a8733
Bernardo Rodriguez <brodriguez@atlassian.com>
2024-12-30 07:32:34 -0800

    add pen cookie

--- assets/product-form.js
+++ assets/product-form.js
@@ -222,8 +222,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         // this.add_pen()
         break
       case 'redirect_ut':
-        // add_pen_cookie()
-        this.add_pen()
+        add_pen_cookie()
         subPrice = '$9'
         subText = 'STARTER SPECIAL'
         // subPrice = '$0'
