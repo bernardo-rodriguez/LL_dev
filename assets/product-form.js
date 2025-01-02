3ce6102d3ccd122fb98bace480a4a543197250a2
Bernardo Rodriguez <brodriguez@atlassian.com>
2025-01-02 12:45:30 -0800

    d

--- assets/product-form.js
+++ assets/product-form.js
@@ -440,6 +440,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
     document.cookie = "directcheckout=true;path=/";
 
     console.log(this.onetime)
+    
     console.log(this.subsave)
 
     const submitButton = this.querySelector('[type="submit"]');
