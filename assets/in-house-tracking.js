eadeaf678ea219642ec6c6f351018ee54f0e3c09
Bernardo Rodriguez <brodriguez@atlassian.com>
2025-02-09 21:54:18 -0500

    upsell for all

--- assets/in-house-tracking.js
+++ assets/in-house-tracking.js
@@ -22,7 +22,7 @@ A_B_testing_campaigns = {
     active_name: 'active',
     inactive_name: 'inactive',
     affiliate_tested: supported_affiliates['sweatcoin'],
-    active_split: '50',
+    active_split: '0',
     page_and_functions: [
       {
         page: 'at-home-whitening-kit-affiliate-ft',
