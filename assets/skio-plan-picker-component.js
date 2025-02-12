0926a4be7425bed24ed2987a635a2ffb2e00edae
Bernardo Rodriguez <brodriguez@atlassian.com>
2025-02-11 22:05:35 -0500

    try this

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -796,7 +796,10 @@ export class SkioPlanPickerComponent extends LitElement {
                   </div>
                   <div class="skio-center-wrapper">
                     <div class="skio-group-title" id = 'skio-group-title-sub'>
-                    ${ group.name == 'Subscription' ?  (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' ? 'Sweatcoin Special' : 'Subscribe & Save' : group.name }
+                    ${ group.name == 'Subscription' ?  (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' ? 
+                      ((this.product.id == 8187028177121 ) ? 'Sweatcoin Special' : 'Free Trial Special') 
+                      : 'Subscribe & Save' 
+                    : group.name }
                       ${ this.discount(group.selected_selling_plan).percent !== '0%' ? html` 
                         <span style="display: none;" class="skio-save">Save <span skio-discount>${ this.discountFormat == 'percent' ? this.discount(group.selected_selling_plan).percent : this.discount(group.selected_selling_plan).amount }</span></span>
                       ` : html`` }
