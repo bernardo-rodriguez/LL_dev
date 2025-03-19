d3b586110faf422f2b04b60e94440681bdb6bc83
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-18 23:23:54 -0400

    what about this

--- assets/in-house-tracking.js
+++ assets/in-house-tracking.js
@@ -239,6 +239,7 @@ function setCookieIfFirstTime(utm_affiliate) {
 
 function setCookieAffiliate(cookie) {
   console.log('hello there??')
+  console.log(cookie)
   setCookie(cookie, 'true')
   setCookie('affiliate_referrer', cookie)
 
