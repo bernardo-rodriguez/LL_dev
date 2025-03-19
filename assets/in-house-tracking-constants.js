7b90d2103ad457716ab573177ee67f21e2197ef7
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-19 10:36:08 -0400

    inhouse tracking constants non deferred

---
+++ assets/in-house-tracking-constants.js
@@ -0,0 +1,167 @@
+affiliate_config = {
+    'default': {
+      'general': {
+        'announcement_bar': null,
+      },
+      'pricing': {
+        'onetime': '59',
+        'subscription': '29',
+        'one_time_enabled': true,
+        'subscription_enabled': true,
+      },
+      'flow': {
+        'product_page': null,
+        'skip_quiz': false,
+        'discount_code': null,
+        'cart_attribute': null,
+      },
+      'landing_page_copy': {
+        'hero_subtitle_1': null,
+        'hero_title': null,
+        'Hero_Subtitle': null,
+        'hero_price': '29',
+      },
+      'featured_product': {
+        'subPrice': '29',
+        'discount_name': 'SUB-N-SAVE',
+        'discount_desc': '75% OFF',
+        'regular_item_price': null
+      }
+    },
+    'redirect_cpgap': {
+      'flow': {
+        'discount_code': 'Pen-Addon',
+        'cart_attribute': 'redirect_cpgap',
+      },
+    },
+    'redirect_cpgap_gen': {
+      'flow': {
+        'discount_code': 'Pen-Addon',
+        'cart_attribute': 'redirect_cpgap_gen',
+      },
+      'landing_page_copy': {
+        'hero_subtitle_1': ['margin-bottom', '50px !important'],
+        'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
+        'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
+        'hero_price': '0',
+      },
+      'featured_product': {
+        'subPrice': '0',
+        'discount_name': 'FREE_TRIAL_SPECIAL',
+        'discount_desc': '100% OFF',
+        'regular_item_price': '$59'
+      }
+    },
+    'redirect_paceline': {
+      'flow': {
+        'discount_code': 'PACELINE_AUTOMATIC',
+        'cart_attribute': 'paceline',
+      },
+      'landing_page_copy': {
+        'hero_price': '29',
+      },
+      'featured_product': {
+        'subPrice': '29'
+      }
+    },
+    'redirect_sweatcoin': {
+      'flow': {
+        'discount_code': 'Pen-Addon',
+        'cart_attribute': 'sweatcoin',
+      },
+      'landing_page_copy': {
+        'hero_subtitle_1': ['margin-bottom', '50px !important'],
+        'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
+        'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
+        'hero_price': '0',
+      },
+      'featured_product': {
+        'subPrice': '0',
+        'discount_name': 'SWEATCOIN_LIMITED',
+        'discount_desc': '100% OFF',
+        'regular_item_price': '$59'
+      }
+    },
+    'redirect_ut': {
+      'flow': {
+        'discount_code': 'ADD_PEN',
+        'cart_attribute': 'cactus_media',
+      },
+      'landing_page_copy': {
+        'hero_subtitle_1': ['margin-bottom', '50px !important'],
+        'hero_title': 'Get A <span class = "stylized">Free Whitening Pen</span> <br> With Your Kit',
+        'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
+        'hero_price': '9',
+      },
+      'featured_product': {
+        'subPrice': '9',
+        'discount_name': 'STARTER_SPECIAL',
+        'discount_desc': '85% OFF',
+        'regular_item_price': '$59'
+      }
+    },
+    'redirect_ut_direct': {
+      'featured_product': {
+        'subPrice': '9'
+      }
+    },
+    'redirect_miles': {
+      'flow': {
+        'discount_code': 'MILES_AUTOMATIC',
+        'cart_attribute': 'miles',
+      },
+      'landing_page_copy': {
+        'hero_price': '9',
+      },
+      'featured_product': {
+        'subPrice': '9',
+        'discount_name': 'MILES_AUTOMATIC',
+        'discount_desc': '88% OFF',
+        'regular_item_price': '$59'
+      }
+    },
+    'redirect_skimm': {
+      'flow': {
+        'discount_code': 'SKIMM25',
+        'cart_attribute': 'skimm',
+      },
+    },
+    'redirect_pinterest':  {
+      'flow': {
+        'discount_code': 'PINTEREST25',
+        'cart_attribute': 'pinterest',
+      }
+    },
+    'jam_media': {
+      'flow': {
+        'discount_code': 'AFFILIATE-DISCOUNT-TJM',
+        'cart_attribute': 'jam_media',
+      },
+      'landing_page_copy': {
+        'hero_subtitle_1': ['margin-bottom', '50px !important'],
+        'hero_price': '19',
+      },
+      'featured_product': {
+        'subPrice': '19',
+        'discount_name': 'Subscribe & Save',
+        'discount_desc': '35% OFF',
+        'regular_item_price': '$59'
+      }
+    },
+    'redirect_studentbeans': {
+      'landing_page_copy': {
+        'hero_price': '9',
+      },
+      'featured_product': {
+        'subPrice': '9'
+      }
+    },
+    'redirect_inspire': {
+      'featured_product': {
+        'subPrice': '13.5',
+        'discount_name': 'Subscribe & Save',
+        'discount_desc': '80% OFF',
+        'regular_item_price': '$59'
+      }
+    }
+  }
