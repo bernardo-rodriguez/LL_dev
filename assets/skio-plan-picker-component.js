2c3b353474f186397bd1cbdce97edd67470d4b07
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-19 20:20:38 -0400

    remove logging

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -570,7 +570,7 @@ export class SkioPlanPickerComponent extends LitElement {
     this.selectedVariant = null;
 
     this.affiliate_referrer = getCookie('affiliate_referrer')
-    console.log(affiliate_config)
+
     this.one_time_enabled = (affiliate_config[this.affiliate_referrer] ?? {}).pricing?.one_time_enabled ?? true;
     this.subscription_enabled = (affiliate_config[this.affiliate_referrer] ?? {}).pricing?.subscription_enabled ?? true;
 
