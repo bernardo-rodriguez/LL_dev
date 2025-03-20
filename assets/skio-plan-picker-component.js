982af5456f326b0a4c691e25c9ed8d94a370bdff
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-19 20:43:00 -0400

    invalid property

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -730,7 +730,7 @@ export class SkioPlanPickerComponent extends LitElement {
                 (this.one_time_enabled && !this.subscription_enabled) || 
                 (this.startSubscription == false && this.product.requires_selling_plan) == false ? true : false }>
 
-              <label skio-label-onetime class="skio-group-label" for="skio-one-time-${ this.key }" style = ${ this.subscription_enabled ? '' : 'padding: none' }>
+              <label skio-label-onetime class="skio-group-label" for="skio-one-time-${ this.key }" style = ${ this.subscription_enabled ? '' : 'padding: 0' }>
                 <div class="skio-group-topline">
                   <div class="skio-radio__container" style = ${ this.subscription_enabled && this.one_time_enabled ? '' : 'display: none'} >
                     <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
