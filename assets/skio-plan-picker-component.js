e539b31a09225a0763ab58bcb6941b265eafa2d4
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-27 15:48:38 -0400

    d

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -834,7 +834,7 @@ export class SkioPlanPickerComponent extends LitElement {
                           <div class="skio-group-title" id = 'skio-group-title-sub'>
                           ${ group.name == 'Subscription' ?  (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' ? 
                             ((this.product.id == 8187028177121 ) ? 'Sweatcoin Special' : 'Free Trial Special') 
-                            : 'Subscribe & Save' 
+                            : 'Starter Kit' 
                           : group.name }
                             ${ this.discount(group.selected_selling_plan).percent !== '0%' ? html` 
                               <span style="display: none;" class="skio-save">Save <span skio-discount>${ this.discountFormat == 'percent' ? this.discount(group.selected_selling_plan).percent : this.discount(group.selected_selling_plan).amount }</span></span>
