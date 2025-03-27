50a34cfc6e1c63bed80a7c24ee9bde7494b2ebb1
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-27 15:50:10 -0400

    d

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -854,8 +854,7 @@ export class SkioPlanPickerComponent extends LitElement {
                       <div class="skio-group-content">
                         <div class="skio-custom-content skio-custom-content-background-color">
                           <div class="skio-container">
-                            <div>${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? 'Whitening Gels' : 'Whitening Gels' }</div>
-                            <div>${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? '1 Month Supply' : 'Refills for $25/delivery' }</div>
+                            <div>'Refills for $25/delivery</div>
                           </div>
                           <div class="skio-container" style = "margin-top: 10px; justify-content: flex-end">
                             <select skio-selling-plans="${ group.id }" class="skio-frequency${ group.selling_plans.length == 1 ? ' skio-frequency--one' : '' }
