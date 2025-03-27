8b66222bce56eaaa4116fdaf18b88c73940e0cfb
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-03-27 15:51:44 -0400

    skio second line

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -854,9 +854,7 @@ export class SkioPlanPickerComponent extends LitElement {
                       <div class="skio-group-content">
                         <div class="skio-custom-content skio-custom-content-background-color">
                           <div class="skio-container">
-                            <div>'Refills for $25/delivery</div>
-                          </div>
-                          <div class="skio-container" style = "margin-top: 10px; justify-content: flex-end">
+                            <div>Refills for $25/delivery</div>
                             <select skio-selling-plans="${ group.id }" class="skio-frequency${ group.selling_plans.length == 1 ? ' skio-frequency--one' : '' }
                             ${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? ' hide-skio-select' : '' }"
                               @change=${ (e) => this.selectSellingPlan(e.target, group) }>
@@ -868,7 +866,6 @@ export class SkioPlanPickerComponent extends LitElement {
                                 `
                               ): ''}
                             </select>
-                            
                           </div>
                         </div>
                       </div>
