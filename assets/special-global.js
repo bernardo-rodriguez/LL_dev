74aa2ebe59a85b8334040224c59032890da816cf
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-16 17:30:00 -0400

    d

--- assets/special-global.js
+++ assets/special-global.js
@@ -1,10 +1,10 @@
-function getFocusableElements(container) {
-  return Array.from(
-    container.querySelectorAll(
-      "summary, a[href], button:enabled, [tabindex]:not([tabindex^='-']), [draggable], area, input:not([type=hidden]):enabled, select:enabled, textarea:enabled, object, iframe"
-    )
-  );
-}
+// function getFocusableElements(container) {
+//   return Array.from(
+//     container.querySelectorAll(
+//       "summary, a[href], button:enabled, [tabindex]:not([tabindex^='-']), [draggable], area, input:not([type=hidden]):enabled, select:enabled, textarea:enabled, object, iframe"
+//     )
+//   );
+// }
 
 class SectionId {
   static #separator = '__';
