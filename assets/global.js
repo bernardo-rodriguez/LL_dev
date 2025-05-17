708b98c8e902e47fad6f6a758e59f51fc3cec3c2
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-17 16:07:01 -0400

    ok

--- assets/global.js
+++ assets/global.js
@@ -582,9 +582,7 @@ var VariantSelects = class extends HTMLElement {
   }
 
   getVariantData() {
-    console.log('hi')
     this.variantData = this.variantData || JSON.parse(this.querySelector('[type="application/json"]').textContent);
-    console.log(this.querySelector('[type="application/json"]'))
     console.log(this.variantData)
     return this.variantData;
   }
