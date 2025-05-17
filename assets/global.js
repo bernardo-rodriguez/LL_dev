43714d61785144abbe297ecb33a4448f1d749260
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-17 16:27:49 -0400

    d

--- assets/global.js
+++ assets/global.js
@@ -559,29 +559,23 @@ var VariantSelects = class extends HTMLElement {
   }
 
   onVariantChange() {
-    console.log('1')
     this.updateOptions();
     this.updateMasterId();
-    console.log('2')
     this.toggleAddButton(true, '', false);
     this.updatePickupAvailability();
     this.removeErrorMessage();
-    console.log('3')
 
     if (!this.currentVariant) {
-      console.log('4')
       this.toggleAddButton(true, '', true);
       this.setUnavailable();
       
     } else {
-      console.log('5')
       this.updateMedia();
       this.updateURL();
       this.updateVariantInput();
       this.renderProductInfo();
       this.updateShareUrl();
     }
-    console.log('6')
   }
 
   updateOptions() {
@@ -612,6 +606,7 @@ var VariantSelects = class extends HTMLElement {
       } else {
         this.currentVariant = variantData
       }
+      console.log(this.currentVariant)
     }
 
   updateMedia() {
