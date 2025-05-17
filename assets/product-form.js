7602d7ae304f721263839d7b854142b71c4c861c
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-17 15:54:00 -0400

    d

--- assets/product-form.js
+++ assets/product-form.js
@@ -174,7 +174,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
 
         console.log(selection)
 
-        let selection = '✨ Everyday (ID: 8-16)';  // or dynamic string
+        selection = '✨ Everyday (ID: 8-16)';  // or dynamic string
         let refill_formula_selector = this.querySelector(`variant-radios input[value="${selection}"]`);
         let refill_formula_new_dawn = this.querySelector(`variant-selects input[type="radio"][value="${selection}"]`);
 
