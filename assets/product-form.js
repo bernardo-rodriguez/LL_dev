4a13234ee2f39b738957646b409cee6bad6ff27a
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-17 12:17:35 -0400

    find?

--- assets/product-form.js
+++ assets/product-form.js
@@ -174,6 +174,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
         console.log(selection)
 
         let refill_formula_selector = this.querySelector(`variant-radios input[value="${selection}"]`)
+        console.log(refill_formula_selector)
         refill_formula_selector.click()
 
         // refill_formula_selector.setAttribute('checked', '');
