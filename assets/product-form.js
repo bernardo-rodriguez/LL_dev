5cf7576ee0eed85dffc98fe07b63c83f9bead281
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-05-17 20:06:13 -0400

    dolla sign

--- assets/product-form.js
+++ assets/product-form.js
@@ -344,7 +344,6 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
             let sub_price = document.querySelector('skio-plan-picker').shadowRoot.querySelector(`[skio-subscription-price]`)?.innerText
             
             $('span.price-item.price-item--regular').html(sub_price + '.00 USD');
-
           });
         });
 
