a140b3110f2b2eb7fe32dada91bf0ca666982364
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-06-23 11:39:36 -0400

    thing

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -858,7 +858,7 @@ export class SkioPlanPickerComponent extends LitElement {
                             ` : html`` }
                             <span skio-subscription-price>$${ this.subscription_pricing != '' ? this.subscription_pricing : 
                             // (this.price(group.selected_selling_plan, false) / 100).toFixed(2) }
-                            (this.affiliate_referrer == 'redirect_ut_trial' ? ((this.price(group.selected_selling_plan, false) / 100)).toFixed(2) : (this.price(group.selected_selling_plan, false) / 100).toFixed(2)) }
+                            (this.affiliate_referrer == 'redirect_ut_trial' ? ((this.price(group.selected_selling_plan, false) / 100) - 4).toFixed(2) : (this.price(group.selected_selling_plan, false) / 100).toFixed(2)) }
                             </span>
                           </div>
                         </div>
