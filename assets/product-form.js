10d377d10b6636f6a5484a6253bc679ffd978e7b
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-06-23 12:11:32 -0400

    do

--- assets/product-form.js
+++ assets/product-form.js
@@ -298,9 +298,13 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
       try {
         let skio_plan_picker = document.querySelector('skio-plan-picker')
         skio_plan_picker.addEventListener('click', (e) => {
-          if (document.querySelector('[data-sticky-onetime]')) {
-            let customPrice = document.querySelector('skio-plan-picker').shadowRoot.querySelector('input[name="onetime_bundle"]:checked').dataset.customPrice
-            document.querySelector(".sticky__price").innerHTML = customPrice
+          try {
+            if (document.querySelector('[data-sticky-onetime]')) {
+              let customPrice = document.querySelector('skio-plan-picker').shadowRoot.querySelector('input[name="onetime_bundle"]:checked').dataset.customPrice
+              document.querySelector(".sticky__price").innerHTML = customPrice
+            }
+          } catch (e) {
+            console.log(e)
           }
         });
       } catch (e) {
