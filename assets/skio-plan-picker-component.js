1b73d99789c8586939e2c11872a17c23901a153a
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-06-28 11:10:23 -0400

    d

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -869,7 +869,8 @@ export class SkioPlanPickerComponent extends LitElement {
                               })()
                             ) }
                             </span>
-                            ${ this.product.id == 7503162605793 && this.discount(group.selected_selling_plan).percent !== '0%' ? 
+                            <!-- comment out for now -->
+                            ${ false && this.product.id == 7503162605793 && this.discount(group.selected_selling_plan).percent !== '0%' ? 
                               html`<span style="font-weight: 500;">— Save ${ this.discount(group.selected_selling_plan).percent }</span>` 
                               : html`` }
                           </div>
