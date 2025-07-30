9e968e7a1ea2a2f6fefe825357eacfcff3a2e79f
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-07-29 21:03:08 -0400

    thng

--- assets/in-house-tracking-constants.js
+++ assets/in-house-tracking-constants.js
@@ -334,7 +334,7 @@ affiliate_config = {
             'product_page': '/products/at-home-whitening-kit-affiliate-ut',
             'cart_attribute': 'cactus_media',
             'show_upsell': true,
-            'discount_code': 'UTMTRIAL,Pen-Addon'
+            'discount_code': 'UTM_GEN_TRIAL,Pen-Addon'
         },
         'landing_page_copy': {
             'hero_subtitle_1': ['margin-bottom', '50px !important'],
