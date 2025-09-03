84a9dfce4915e5184f03cd7732cdaeb2639d4e36
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-02 21:20:10 -0400

    do the thing

---
+++ assets/in-house-constants.js
@@ -0,0 +1,44 @@
+in_house_products = {
+    'KIT_DOUBLE_LIGHTNING': {
+        'product_id': 8187028177121,
+        'variants': {
+            'everyday': 44311331045601,
+            'gentle': 44311331111137,
+            'super': 44311331078369,
+        }
+    },
+    'KIT_EMPTY_SPACE': {
+        'product_id': 8252255568097,
+        'variants': {
+            'everyday': 44522540335329,
+            'gentle': 44522540400865,
+            'super': 44522540368097,
+        }
+    },
+    'KIT_DEFAULT': {
+        'product_id': 7503162605793,
+        'variants': {
+            'everyday': 42210192457953,
+            'gentle': 42210192425185,
+            'super': 42210192490721,
+        }
+    },
+    'REFILL_DEFAULT': {
+        'product_id': 7498061906145,
+        'variants': {
+            'everyday': 42198066987233,
+            'gentle': 42198066954465,
+            'super': 42198067020001,
+            'diablo': 42198067052769
+        }
+    },
+    'PEN_DEFAULT': {
+        'product_id': 7503266611425,
+        'variants': {
+            'everyday': 42210600812769,
+            'gentle': 42210600878305,
+            'super': 42210600845537,
+        }
+    }
+
+}
