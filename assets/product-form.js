827554b74f82d839297732d97bf7ef80ffd52863
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-05 09:17:59 -0400

    and this

--- assets/product-form.js
+++ assets/product-form.js
@@ -59,7 +59,7 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
   async initializeSellingPlans() {
     try {
       if (this.productId) {
-        this.sellingPlans = await fetchPlansByProductIds([this.productId]);
+        this.sellingPlans = await fetchPlansByProductIds([this.productId, window.ProductConfig.REFILL_DEFAULT.product_id]);
         console.log('Selling plans loaded:', this.sellingPlans);
         this.indexPlansByVariant(this.sellingPlans);
       }
