f105ef0480668ff9483bf6941de1013eca4fdce5
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-05 15:57:08 -0400

    d

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -1004,7 +1004,7 @@ export class SkioPlanPickerComponent extends LitElement {
                       ${ this.product.id != window.ProductConfig.KIT_DOUBLE_LIGHTNING.product_id 
                       && this.product.id != window.ProductConfig.KIT_EMPTY_SPACE.product_id ?
                         html`
-                        <div class="skio-group-content skio-custom-content-background-color; border-radius: 8px">
+                        <div class="skio-group-content skio-custom-content-background-color" style= "border-radius: 8px">
                               <div class="bundle-option" data-bundle="1" style = "border: none; margin-bottom: 0">
                                 <div class="bundle-content">
                                   <input type="radio" name="onetime_bundle" value="1" data-custom-price="${ this.oneTimePricingConfig['first']['bottom_left'] }" checked>
