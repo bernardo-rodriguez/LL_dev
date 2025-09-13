85f3b5fa28e8b1e229377ad76d1c5dd835acd0a8
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-13 17:15:53 -0400

    comment

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -875,6 +875,7 @@ export class SkioPlanPickerComponent extends LitElement {
                           </div>
                         </div>
 
+                        <!--
                         <div class="bundle-option" data-bundle="3" style = 'margin-bottom: 0;'>
                           <div class="bundle-content">
                             <input type="radio" name="onetime_bundle" value="3" data-custom-price="${ this.oneTimePricingConfig['third']['bottom_left'] }" >
@@ -897,6 +898,8 @@ export class SkioPlanPickerComponent extends LitElement {
                             </div>
                           </div>
                         </div>
+                        -->
+                        
                       </div>
                     </div>
                     
