acfe556cc71406cce900eee3a49c306223fb3923
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-14 23:30:06 -0400

    do it

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -986,9 +986,9 @@ export class SkioPlanPickerComponent extends LitElement {
                       && this.product.id != window.ProductConfig.KIT_EMPTY_SPACE.product_id ?
                         html`
                         <div class="skio-group-content skio-custom-content-background-color" style= "border-radius: 8px; margin-top: 10px;">
-                              <div class="bundle-option"style = "margin-bottom: 0">
+                              <div class="bundle-option" style = "margin-bottom: 0">
                                 <div class="bundle-content">
-                                  <input type="radio" checked>
+                                  <input type="radio" name="onetime_bundle" value = "sub" checked>
                                   <div class="bundle-details">
                                     <div class="bundle-header">
                                       <div>
