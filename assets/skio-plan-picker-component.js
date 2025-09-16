1409b1e9725b5855f3d23c842fe2294e094a0af9
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-09-15 23:37:58 -0400

    do it

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -1032,6 +1032,40 @@ export class SkioPlanPickerComponent extends LitElement {
 
                       ${ this.product.id != window.ProductConfig.KIT_DOUBLE_LIGHTNING.product_id 
                       && this.product.id != window.ProductConfig.KIT_EMPTY_SPACE.product_id ?
+                        (this.product.id == window.ProductConfig.REFILL_DEFAULT.product_id) ?      
+                        html`<div class="skio-group-content">
+                          <div class="skio-custom-content skio-custom-content-background-color">
+                            <div class="skio-container">
+                              <div> ${ this.affiliate_referrer == 'redirect_ut' ? 
+                                "Refills for $30" 
+                                : 
+                                "Refills for $30" }
+                              </div>
+                              <select skio-selling-plans="${ group.id }" class="skio-frequency
+                              ${ group.selling_plans.length == 1 ? 
+                               ' skio-frequency--one' 
+                               : 
+                               ''
+                              }
+                              ${ (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '0' 
+                              || (this.price(group.selected_selling_plan, false) / 100).toFixed(0) == '9' ? 
+                                ' hide-skio-select' 
+                                : 
+                                '' 
+                              }"
+                                @change=${ (e) => this.selectSellingPlan(e.target, group) }>
+                                ${ group ? group.selling_plans.map((selling_plan) => 
+                                  html`
+                                  <option value="${ selling_plan.id }" ?selected=${group.selected_selling_plan == selling_plan }>
+                                    ${ group.name == 'Subscription' ? `Delivery ${ selling_plan.name.toLowerCase() }` : `${ selling_plan.name }` }
+                                  </option>
+                                  `
+                                ): ''}
+                              </select>
+                            </div>
+                          </div>
+                        </div>`
+                        :
                         html`
                         <div class="skio-group-content ${this.selectedBundle === 'sub' ? 'skio-custom-content-background-color' : ''}" style= "border-radius: 8px; margin-top: 10px;">
                               <div class="bundle-option" style = "margin-bottom: 0">
