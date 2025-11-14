02e5ec61a0299814dae4e1e790781ddd270ba7ce
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-11-13 19:43:23 -0500

    pen

--- assets/in-house-tracking-constants.js
+++ assets/in-house-tracking-constants.js
@@ -298,75 +298,75 @@ affiliate_config = {
             'regular_item_price': '$59'
         }
     },
-    'redirect_ut': {
-        'pricing': {
-            'subscription_enabled': true,
-            'one_time_enabled': false,
-            'bundle_enabled': false,
-            'subscription_discount': '6',
-        },
-        'general': {
-            'announcement_bar': 'Discount auto applied at checkout!',
-        },
-        'product_page_copy': {
-            'bundle_offer_title': 'Starter Kit',
-        },
-        'flow': {
-            'product_page': '/products/at-home-whitening-kit-affiliate-ut',
-            'cart_attribute': 'cactus_media',
-            'show_upsell': true,
-            'discount_code': 'UTM_GEN_TRIAL, Pen-Addon,UTM_SUB_12_TREATMENTS,UTM_SUB_12_TREATMENTS_STRONG'
-        },
-        'landing_page_copy': {
-            'hero_subtitle_1': ['margin-bottom', '50px !important'],
-            'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
-            // 'hero_title': 'Get A <span class = "stylized">Free Whitening Pen</span> <br> With Your Kit',
-            'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
-            // 'hero_price': '9',
-            'hero_price': '0',
-        },
-        'featured_product': {
-            'subPrice': '0',
-            // 'subPrice': '9',
-            'discount_name': 'FREE_TRIAL_SPECIAL',
-            // 'discount_name': 'STARTER_SPECIAL',
-            'discount_desc': '100% OFF',
-            // 'discount_desc': '85% OFF',
-            'regular_item_price': '$59'
-        }
-    },
     // 'redirect_ut': {
     //     'pricing': {
+    //         'subscription_enabled': true,
     //         'one_time_enabled': false,
-    //         'subscription_enabled': true
-    //         // 'subscription': '4.95'
+    //         'bundle_enabled': false,
+    //         'subscription_discount': '6',
     //     },
     //     'general': {
     //         'announcement_bar': 'Discount auto applied at checkout!',
+    //     },
+    //     'product_page_copy': {
+    //         'bundle_offer_title': 'Starter Kit',
     //     },
     //     'flow': {
     //         'product_page': '/products/at-home-whitening-kit-affiliate-ut',
     //         'cart_attribute': 'cactus_media',
     //         'show_upsell': true,
-    //         'discount_code': 'UTM_GEN_TRIAL,Pen-Addon'
+    //         'discount_code': 'UTM_GEN_TRIAL, Pen-Addon,UTM_SUB_12_TREATMENTS,UTM_SUB_12_TREATMENTS_STRONG'
     //     },
     //     'landing_page_copy': {
     //         'hero_subtitle_1': ['margin-bottom', '50px !important'],
-    //         // 'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
+    //         'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
     //         // 'hero_title': 'Get A <span class = "stylized">Free Whitening Pen</span> <br> With Your Kit',
-    //         // 'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
-    //         'hero_price': '9',
-    //         // 'hero_price': '0',
+    //         'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
+    //         // 'hero_price': '9',
+    //         'hero_price': '0',
     //     },
     //     'featured_product': {
-    //         // 'subPrice': '0',
-    //         'subPrice': '9',
-    //         // 'discount_name': 'FREE_TRIAL_SPECIAL',
-    //         'discount_name': 'STARTER_SPECIAL',
-    //         'discount_desc': '85% OFF',
+    //         'subPrice': '0',
+    //         // 'subPrice': '9',
+    //         'discount_name': 'FREE_TRIAL_SPECIAL',
+    //         // 'discount_name': 'STARTER_SPECIAL',
+    //         'discount_desc': '100% OFF',
+    //         // 'discount_desc': '85% OFF',
     //         'regular_item_price': '$59'
     //     }
     // },
+    'redirect_ut': {
+        'pricing': {
+            'one_time_enabled': false,
+            'subscription_enabled': true
+            // 'subscription': '4.95'
+        },
+        'general': {
+            'announcement_bar': 'Discount auto applied at checkout!',
+        },
+        'flow': {
+            'product_page': '/products/at-home-whitening-kit-affiliate-ut',
+            'cart_attribute': 'cactus_media',
+            'show_upsell': false,
+            'discount_code': 'pen-addon'
+        },
+        'landing_page_copy': {
+            'hero_subtitle_1': ['margin-bottom', '50px !important'],
+            // 'hero_title': 'Try Dentist-Made <br> Teeth Whitening <br> <span class = "stylized">For Free Today</span>',
+            // 'hero_title': 'Get A <span class = "stylized">Free Whitening Pen</span> <br> With Your Kit',
+            // 'Hero_Subtitle': "See A Noticeable Difference In Just One Week With A Customized Whitening Solution Today.",
+            'hero_price': '9',
+            // 'hero_price': '0',
+        },
+        'featured_product': {
+            // 'subPrice': '0',
+            'subPrice': '9',
+            // 'discount_name': 'FREE_TRIAL_SPECIAL',
+            'discount_name': 'STARTER_SPECIAL',
+            'discount_desc': '85% OFF',
+            'regular_item_price': '$59'
+        }
+    },
     // 'redirect_ut_trial': {
     //     'pricing': {
     //         'one_time_enabled': false,
