18ff228104b6a3d976823e9150e5218612a54850
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2025-11-13 19:48:41 -0500

    d

--- assets/product-form.js
+++ assets/product-form.js
@@ -496,13 +496,18 @@ customElements.define('product-form', class ProductForm extends HTMLElement {
       }
     }
 
-    if (product_form.product_id == window.ProductConfig.KIT_ONE_MONTH_SUPPLY.product_id || product_form.product_id == window.ProductConfig.KIT_EMPTY_SPACE) {
+    console.log('thingy')
+    console.log(product_form.product_id)
+    console.log(window.ProductConfig.KIT_EMPTY_SPACE)
+    if (product_form.product_id == window.ProductConfig.KIT_ONE_MONTH_SUPPLY.product_id || product_form.product_id == window.ProductConfig.KIT_EMPTY_SPACE.product_id) {
+      console.log('adding pen cookie')
       add_pen_cookie()
       // setCookie('productDiscountCode', 'ADD_PEN')
     }
 
     let pen = this.getCookie('add_pen')
-    if ((pen && pen != 'false') || (product_form.product_id == window.ProductConfig.KIT_ONE_MONTH_SUPPLY.product_id || product_form.product_id == window.ProductConfig.KIT_EMPTY_SPACE)) {
+    console.log(pen)
+    if ((pen && pen != 'false') || (product_form.product_id == window.ProductConfig.KIT_ONE_MONTH_SUPPLY.product_id || product_form.product_id == window.ProductConfig.KIT_EMPTY_SPACE.product_id)) {
       itemsList.push({
         id: pen,
         quantity: 1
